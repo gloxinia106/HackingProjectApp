848ad8bd0a59a1454cddbad1e04011049dcfffaf
gloxinia106 <gloxinia106@gmail.com>
2024-03-26 13:31:39 +0900

    initalize
    first run

---
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.webviewtest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient()
+        myWebView.webChromeClient = WebChromeClient()
+        myWebView.loadUrl("http://192.168.14.180:8080")
+
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val myWebView: WebView = findViewById(R.id.webview)
+            if(myWebView.canGoBack()){
+                myWebView.goBack()
+            }
+            else {
+                finish()
+            }
+        }
+    }
+}
