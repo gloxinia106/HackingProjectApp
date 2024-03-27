44d3a8ee1dede2c20192d08e3bc211ca2e15ab46
gloxinia106 <gloxinia106@gmail.com>
2024-03-27 13:03:47 +0900

    파일 다운로드 기능 구현

--- app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.webviewtest
 
 import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.webkit.CookieManager
+import android.webkit.URLUtil
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -28,8 +34,24 @@ class MainActivity : AppCompatActivity() {
         myWebView.settings.javaScriptEnabled = true
         myWebView.webViewClient = WebViewClient()
         myWebView.webChromeClient = WebChromeClient()
-        myWebView.loadUrl("http://192.168.14.180:8080")
+        myWebView.loadUrl("http://192.168.14.180:3000")
 
+        myWebView.setDownloadListener {url, userAgent, contentDisposition, mimetype, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimetype)
+            val cookies = CookieManager.getInstance().getCookie(url)
+            request.addRequestHeader("cookie", cookies)
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype))
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype))
+
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+        }
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
@@ -43,4 +65,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }
