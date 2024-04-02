4658594aaa050e87c0e490fa2e3832fff9243453
gloxinia106 <gloxinia106@gmail.com>
2024-04-02 11:07:03 +0900

    jwt관련 sharedPreperance 추가
    수정중

--- app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.webviewtest
 
 import android.annotation.SuppressLint
 import android.app.DownloadManager
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.webkit.CookieManager
+import android.webkit.JavascriptInterface
 import android.webkit.URLUtil
 import android.webkit.WebChromeClient
 import android.webkit.WebView
@@ -18,6 +20,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val JWT_STORAGE = "jwt_storage"
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,24 +37,28 @@ class MainActivity : AppCompatActivity() {
         myWebView.settings.javaScriptEnabled = true
         myWebView.webViewClient = WebViewClient()
         myWebView.webChromeClient = WebChromeClient()
-        myWebView.loadUrl("http://192.168.14.180:3000")
+        myWebView.loadUrl("http://192.168.14.180:8080")
+        myWebView.addJavascriptInterface(WebAppInterface(this), "Android")
 
-        myWebView.setDownloadListener {url, userAgent, contentDisposition, mimetype, contentLength ->
-            val request = DownloadManager.Request(Uri.parse(url))
-            request.setMimeType(mimetype)
-            val cookies = CookieManager.getInstance().getCookie(url)
-            request.addRequestHeader("cookie", cookies)
-            request.addRequestHeader("User-Agent", userAgent)
-            request.setDescription("Downloading file...")
-            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype))
-            request.allowScanningByMediaScanner()
-            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype))
-
-            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-            dm.enqueue(request)
-            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
-        }
+//        myWebView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+//            val request = DownloadManager.Request(Uri.parse(url))
+//            request.setMimeType(mimetype)
+//            val cookies = CookieManager.getInstance().getCookie(url)
+//            request.addRequestHeader("cookie", cookies)
+//            request.addRequestHeader("User-Agent", userAgent)
+//            request.setDescription("Downloading file...")
+//            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype))
+//            request.allowScanningByMediaScanner()
+//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+//            request.setDestinationInExternalPublicDir(
+//                Environment.DIRECTORY_DOWNLOADS,
+//                URLUtil.guessFileName(url, contentDisposition, mimetype)
+//            )
+//
+//            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+//            dm.enqueue(request)
+//            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+//        }
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
@@ -64,6 +71,35 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    class WebAppInterface(private val mContext : Context) {
+        // 웹에서 안드로이드 toastmessage 띄우기!!?
+        @JavascriptInterface
+        fun saveJWT(token:String){
+            (mContext as MainActivity).writeJwtSharedPreference("jwt",token)
+            val toast = Toast.makeText(mContext,"$token SAVE",Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        @JavascriptInterface
+        fun loadJWT(){
+            val token = (mContext as MainActivity).readJwtSharedPreference("jwt")
+            val toast = Toast.makeText(mContext,token,Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+    fun writeJwtSharedPreference(key:String,value:String) {
+        val sp = getSharedPreferences(JWT_STORAGE,Context.MODE_PRIVATE)
+        val editor = sp.edit()
+        editor.putString(key,value)
+        editor.apply()
+    }
+
+    fun readJwtSharedPreference(key:String):String{
+        val sp = getSharedPreferences(JWT_STORAGE,Context.MODE_PRIVATE)
+        return sp.getString(key,"") ?: ""
     }
 
 }
