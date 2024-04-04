0e064da252c1c5865a241faa283238968eddd4cc
gloxinia106 <gloxinia106@gmail.com>
2024-04-04 14:28:46 +0900

    파일 업로드 다운로드 끝!

--- app/src/main/java/com/example/webviewtest/MainActivity.kt
+++ app/src/main/java/com/example/webviewtest/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.webviewtest
 
+import android.R.attr.mimeType
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.DownloadManager
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.webkit.CookieManager
 import android.webkit.JavascriptInterface
 import android.webkit.URLUtil
+import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -19,8 +23,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
-    val JWT_STORAGE = "jwt_storage"
+    val STORAGE = "storage"
+    private var filePathCallback: ValueCallback<Array<Uri>>? = null
+    private val fileRequestCode = 1
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,30 +42,101 @@ class MainActivity : AppCompatActivity() {
 
         val myWebView: WebView = findViewById(R.id.webview)
         myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.domStorageEnabled = true
+
         myWebView.webViewClient = WebViewClient()
-        myWebView.webChromeClient = WebChromeClient()
-        myWebView.loadUrl("http://192.168.14.180:8080")
+        myWebView.webChromeClient = object : WebChromeClient() {
+            // For Android 5.0+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: FileChooserParams
+            ): Boolean {
+                this@MainActivity.filePathCallback?.onReceiveValue(null)
+                this@MainActivity.filePathCallback = filePathCallback
+
+                val intent = fileChooserParams.createIntent()
+                try {
+                    startActivityForResult(intent, fileRequestCode)
+                } catch (e: Exception) {
+                    this@MainActivity.filePathCallback = null
+                    return false
+                }
+
+                return true
+            }
+        }
+
+//        myWebView.loadUrl("http://16.16.238.235:8080")
+//        myWebView.loadUrl("http://10.0.2.2:3000")
+        myWebView.loadUrl("http://192.168.14.7:8082")
         myWebView.addJavascriptInterface(WebAppInterface(this), "Android")
 
-//        myWebView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
-//            val request = DownloadManager.Request(Uri.parse(url))
-//            request.setMimeType(mimetype)
-//            val cookies = CookieManager.getInstance().getCookie(url)
-//            request.addRequestHeader("cookie", cookies)
-//            request.addRequestHeader("User-Agent", userAgent)
-//            request.setDescription("Downloading file...")
-//            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype))
-//            request.allowScanningByMediaScanner()
-//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-//            request.setDestinationInExternalPublicDir(
-//                Environment.DIRECTORY_DOWNLOADS,
-//                URLUtil.guessFileName(url, contentDisposition, mimetype)
-//            )
-//
-//            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-//            dm.enqueue(request)
-//            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
-//        }
+        myWebView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            var fileName = contentDisposition
+            if (fileName != null && fileName.isNotEmpty()) {
+                val idxFileName = fileName.indexOf("filename=")
+                if (idxFileName > -1) {
+                    fileName = fileName.substring(idxFileName + 9).trim { it <= ' ' }
+                }
+                if (fileName.endsWith(";")) {
+                    fileName = fileName.substring(0, fileName.length - 1)
+                }
+                if (fileName.startsWith("\"") && fileName.startsWith("\"")) {
+                    fileName = fileName.substring(1, fileName.length - 1)
+                }
+            } else {
+                // 파일명(확장자포함) 확인이 안되었을 때 기존방식으로 진행
+                fileName = URLUtil.guessFileName(url, contentDisposition, mimetype)
+            }
+            val adjustedMimeType = if (mimetype.isNullOrEmpty() || mimetype == "application/octet-stream") {
+                guessMimeTypeFromUrl(url)
+            } else {
+                mimetype
+            }
+            request.setMimeType(adjustedMimeType)
+            val cookies = CookieManager.getInstance().getCookie(url)
+            request.addRequestHeader("cookie", cookies)
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(fileName)
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalPublicDir(
+                Environment.DIRECTORY_DOWNLOADS,
+                fileName
+            )
+
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(applicationContext, "다운로드 파일", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun guessMimeTypeFromUrl(url: String): String {
+        // URL에서 파일 확장자 추출 및 MIME 타입 추측 로직 구현
+        // 이 부분은 실제 파일 유형과 URL 구조에 따라 달라질 수 있음
+        return when {
+            url.endsWith(".jpg") || url.endsWith(".jpeg") -> "image/jpeg"
+            url.endsWith(".png") -> "image/png"
+            // 기타 다른 파일 확장자에 대한 처리
+            else -> "application/octet-stream"
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == fileRequestCode) {
+            if (resultCode == Activity.RESULT_OK) {
+                val result = if (data == null || resultCode != Activity.RESULT_OK) null else data.data
+                filePathCallback?.onReceiveValue(arrayOf(Uri.parse(result.toString())))
+            } else {
+                filePathCallback?.onReceiveValue(null)
+            }
+            filePathCallback = null
+        }
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
@@ -74,31 +152,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     class WebAppInterface(private val mContext : Context) {
-        // 웹에서 안드로이드 toastmessage 띄우기!!?
         @JavascriptInterface
-        fun saveJWT(token:String){
-            (mContext as MainActivity).writeJwtSharedPreference("jwt",token)
-            val toast = Toast.makeText(mContext,"$token SAVE",Toast.LENGTH_SHORT)
-            toast.show()
+        fun saveMobileStorage(key:String,value:String){
+            (mContext as MainActivity).writeJwtSharedPreference(key,value)
         }
 
         @JavascriptInterface
-        fun loadJWT(){
-            val token = (mContext as MainActivity).readJwtSharedPreference("jwt")
-            val toast = Toast.makeText(mContext,token,Toast.LENGTH_SHORT)
-            toast.show()
+        fun loadMobileStorage(key:String):String{
+            val mobileVal = (mContext as MainActivity).readJwtSharedPreference(key)
+            return mobileVal
         }
     }
 
     fun writeJwtSharedPreference(key:String,value:String) {
-        val sp = getSharedPreferences(JWT_STORAGE,Context.MODE_PRIVATE)
+        val sp = getSharedPreferences(STORAGE,Context.MODE_PRIVATE)
         val editor = sp.edit()
         editor.putString(key,value)
         editor.apply()
     }
 
     fun readJwtSharedPreference(key:String):String{
-        val sp = getSharedPreferences(JWT_STORAGE,Context.MODE_PRIVATE)
+        val sp = getSharedPreferences(STORAGE,Context.MODE_PRIVATE)
         return sp.getString(key,"") ?: ""
     }
 
